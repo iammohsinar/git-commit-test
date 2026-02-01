10daa89bb9c95c6d0eea5a53691e37c7be7dc60e
Mohsin AR <mohsinazeemrind@gmail.com>
2026-02-01 20:23:48 +0500

    welcome added..!

--- src/main/java/com/my/util/Main.java
+++ src/main/java/com/my/util/Main.java
@@ -7,7 +7,7 @@ public class Main {
   public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
+    welcome();
     hi();
    newFeature();
    goodBye();
@@ -32,4 +32,9 @@ public class Main {
   private static void hi() {
     System.out.println("hi..!");
   }
+
+  private static void welcome() {
+    System.out.printf("Hello and welcome!");
+  }
+
 }
