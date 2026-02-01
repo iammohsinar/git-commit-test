f37c69c0bbda5306e4a772795224e024fea2b9c9
Mohsin AR <mohsinazeemrind@gmail.com>
2026-02-01 20:51:53 +0500

    Reapply "added hello everyone"
    This reverts commit f9efbd0384c0cbd4a616a1e74f2f9038771ed49d.

--- src/main/java/com/my/util/Main.java
+++ src/main/java/com/my/util/Main.java
@@ -5,6 +5,7 @@ package com.my.util;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("Hello everyone..!!");
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     welcome();
